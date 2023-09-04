ad6c84c13a2ec15a352d226acb74d8cbe744a869
JunhwanKang <wnsghks1017@naver.com>
2023-09-04 21:49:34 +0900

    BOARD 추가

---
+++ src/main/java/com/example/cm/data/entity/Board.java
@@ -0,0 +1,35 @@
+package com.example.cm.data.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "board")
+@Getter
+@Setter
+public class Board {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // PK
+    @Column(nullable = true)
+    private String title; // 제목
+    @Column(nullable = true)
+    private String content; // 내용
+    @Column(nullable = true)
+    private String writer; // 작성자
+    @Column(nullable = true)
+    private int view_cnt; // 조회 수
+    @Column(nullable = true)
+    private char delete_yn; // 삭제 여부
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
